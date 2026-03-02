e9eafa7372e57f264ab1737a54f0d6736371b259
uneven74 <ahtnezev@gmail.com>
2026-03-01 23:18:25 -0600

    [add] simulate a state of "custom hook"

---
+++ src/counter/components/MyCounterApp2.test.tsx
@@ -0,0 +1,48 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { MyCounterApp } from "./MyCounterApp";
+// import { useCounter } from "../hooks/useCounter";
+
+const handleAddMock = vi.fn();
+const handleSubstractMock = vi.fn();
+const handleResetMock = vi.fn();
+
+vi.mock("../hooks/useCounter", () => ({
+    useCounter: () => ({
+        counter: 40,
+        handleAdd: handleAddMock,
+        handleSubstract: handleSubstractMock,
+        handleReset: handleResetMock,
+    })
+}));
+
+
+describe("MyCounterApp #2", ()=>{
+
+    test("should render the component", ()=> {
+        render(<MyCounterApp />)
+
+    // screen.debug();
+    expect(screen.getByRole('heading', {level: 1}).innerHTML).toContain(
+      `counter: 40`
+    );
+
+    expect(screen.getByRole('button', {name: '+1'}).innerHTML).toBeDefined();
+    expect(screen.getByRole('button', {name: '-1'}).innerHTML).toBeDefined();
+    expect(screen.getByRole('button', {name: 'Reset'}).innerHTML).toBeDefined();
+    });
+
+    test("should call handledAdd if button is clicked", () => {
+        render(<MyCounterApp/>)
+
+        const button = screen.getByRole('button', {name:'+1'});
+
+        fireEvent.click(button);
+
+        expect(handleAddMock).toHaveBeenCalled();
+        expect(handleAddMock).toHaveBeenCalledTimes(1);
+        expect(handleSubstractMock).not.toHaveBeenCalled();
+        expect(handleResetMock).not.toHaveBeenCalled();
+    });
+
+});
